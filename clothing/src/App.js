b6d290de55f5db4905354c04956c32479ff05626
saideep1011 <saideep2311@gmail.com>
2020-05-11 12:35:09 +0530

    update components to navigate using reactrouter-dom

--- clothing/src/App.js
+++ clothing/src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
-import Homepage from "./pages/homepage/hompage";
+
+import HomePage from "./pages/homepage/hompage";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
